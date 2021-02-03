1786a046250830db5fefcf05e4b4980fe685c11d
Joshuafrankle <joshuafrankle7@gmail.com>
2021-02-03 09:48:03 +0530

    Remove config

--- assets/js/scripts/signup.js
+++ assets/js/scripts/signup.js
@@ -1,15 +1,3 @@
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-// var firebaseConfig = {
-//   apiKey: "AIzaSyBHPx5ij0m6J8LdAZlpzCAjoF2mJDDC2hk",
-//   authDomain: "stretch-pattarai.firebaseapp.com",
-//   projectId: "stretch-pattarai",
-//   storageBucket: "stretch-pattarai.appspot.com",
-//   messagingSenderId: "1046668280702",
-//   appId: "1:1046668280702:web:a17d1559f809831d8e692f",
-//   measurementId: "G-LS9VD8ZZQC",
-// };
 var name,
   email_user,
   photoUrl_user,
--- assets/js/scripts/slot.js
+++ assets/js/scripts/slot.js
@@ -1,18 +1,3 @@
-// var firebaseConfig = {
-//   apiKey: "AIzaSyBHPx5ij0m6J8LdAZlpzCAjoF2mJDDC2hk",
-//   authDomain: "stretch-pattarai.firebaseapp.com",
-//   projectId: "stretch-pattarai",
-//   storageBucket: "stretch-pattarai.appspot.com",
-//   messagingSenderId: "1046668280702",
-//   appId: "1:1046668280702:web:a17d1559f809831d8e692f",
-//   measurementId: "G-LS9VD8ZZQC",
-// };
-// // Initialize Firebase
-// if (!firebase.apps.length) {
-//   firebase.initializeApp(firebaseConfig);
-// }
-// firebase.analytics();
-//get elements
 const auth = firebase.auth();
 firebase.auth().onAuthStateChanged(function (userauth) {
   if (userauth) {
